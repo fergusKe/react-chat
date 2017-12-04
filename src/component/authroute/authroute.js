766a1c160b985d886477fc010e94062b7d5f74af
Fergus <fergus.jingtai@gmail.com>
2017-12-04 18:16:25 +0800

    6-4 登录注册-用户信息校验，跳转登录

--- src/component/authroute/authroute.js
+++ src/component/authroute/authroute.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { withRouter } from 'react-router-dom'
+@withRouter  // 讓 this.props.history 可以抓到值
 
 class AuthRoute extends Component {
 	componentDidMount() {
+		const publicList = ['/login', '/register']
+		const pathname = this.props.location.pathname
+
+		if (publicList.indexOf(pathname) > -1) {
+			return null
+		}
+		
 		// 获取用户信息
 		axios.get('/user/info')
 			.then(res => {
 				if (res.status === 200) {
+					if (res.data.code === 0) {
+						// 有登錄信息
+					} else {
+						console.log(this.props.history)
+						this.props.history.push('/login')
+					}
 					console.log(res.data)
 				}
 			})
