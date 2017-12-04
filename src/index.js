4ef50d03bce70e514f4b6aeffc17f6f40d7e009b
Fergus <fergus.jingtai@gmail.com>
2017-12-04 15:30:59 +0800

    setting router & redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
+
 import App from './App'
+import reducers from './reducer'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(reducers, compose(
+	applyMiddleware(thunk),
+	window.devToolsExtension ? window.devToolsExtension(): f => f
+))
+
+ReactDOM.render(
+	<Provider store={store}>
+		<BrowserRouter>
+			<div>
+				<Route path="/" component={App}></Route>
+			</div>
+		</BrowserRouter>
+	</Provider>,
+	document.getElementById('root')
+)
+
+registerServiceWorker()
