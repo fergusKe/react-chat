b8cf0c7e22bbf6507634aac60568c58a31e7eac7
fergusKe <fergus.jingtai@gmail.com>
2018-02-12 02:00:26 +0800

    6-8 登录注册-express注册功能实现

--- server/user.js
+++ server/user.js
@@ -9,6 +9,22 @@ Router.get('/list', function(req, res) {
 	})
 })
 
+Router.post('/register', function(req, res) {
+	console.log(req.body.data)
+	const { user, pwd, type } = req.body.data
+	User.findOne({ user }, function(err, doc) {
+		if (doc) {
+			return res.json({ code: 1, msg: '用戶名重複'})
+		}
+		User.create({ user, pwd, type }, function(e, d) {
+			if (e) {
+				return res.json({ code: 1, msg: '後端出錯了'})
+			}
+			return res.json({ code: 0 })
+		})
+	})
+})
+
 Router.get('/info', function(req, res) {
 	// 用戶有沒有 cookie
 	return res.json({ code: 1 })
